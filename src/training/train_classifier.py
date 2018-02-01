e6dfdff1e584a5014177aae3d890f187fe882f14
zzhang <zhenxing.zhang@zalando.ie>
2018-02-01 12:24:51 +0000

    update code to write summuries

--- src/training/train_classifier.py
+++ src/training/train_classifier.py
@@ -9,17 +9,26 @@ from src.data_preparation import dataset
 from nets import inception
 
 import tensorflow as tf
+import os
+import datetime
 
 
 def train():
     # This might take a few minutes.
-    train_dir = '/data/checkpoints/flower/'
+    TRAIN_SUMMARY_DIR = "/data/summary/flowers/train"
+    l_rate = 0.001
+    CHECKPOINT_DIR = '/data/checkpoints/flowers/'
+    model_name = "slim_inception_v1"
     flowers_data_dir = "/data/flowers"
     batch_size = 64
 
-    print('Will save model to %s' % train_dir)
+    checkpoint_dir = os.path.join(CHECKPOINT_DIR, model_name, str(l_rate))
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
 
-    with tf.Graph().as_default():
+    print('Will save model to %s' % checkpoint_dir)
+
+    with tf.Graph().as_default() as graph:
         tf.logging.set_verbosity(tf.logging.INFO)
 
         image_size = inception.inception_v1.default_image_size
@@ -28,7 +37,7 @@ def train():
         images, labels = dataset.load_batch(train_dataset, batch_size,
                                             height=image_size, width=image_size, is_training=True)
 
-        tf.summary.image('images', images)
+        tf.summary.image('images/train', images)
 
         # Create the model:
         with slim.arg_scope(inception.inception_v1_arg_scope()):
@@ -40,25 +49,30 @@ def train():
         total_loss = slim.losses.get_total_loss()
 
         # Create some summaries to visualize the training process:
-        tf.summary.scalar('losses/train_Loss', total_loss)
+        tf.summary.scalar('losses/total_loss', total_loss)
 
         with tf.name_scope('accuracy'):
             with tf.name_scope('prediction'):
                 correct_prediction = tf.equal(tf.argmax(logits, 1), labels)
             with tf.name_scope('accuracy'):
                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            tf.summary.scalar('train_accuracy', accuracy)
+            tf.summary.scalar('accuracy', accuracy)
 
         # Specify the optimizer and create the train op:
-        optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+        optimizer = tf.train.AdamOptimizer(learning_rate=l_rate)
         train_op = slim.learning.create_train_op(total_loss, optimizer)
+
+        train_summ_writer = tf.summary.FileWriter(
+            os.path.join(TRAIN_SUMMARY_DIR, model_name, str(l_rate),
+                         datetime.datetime.now().strftime("%Y%m%d-%H%M")), graph)
 
         # Run the training:
         final_loss = slim.learning.train(
             train_op,
-            logdir=train_dir,
+            logdir=checkpoint_dir,
             number_of_steps=1000,  # For speed, we just do 1 epoch
-            save_summaries_secs=1)
+            save_summaries_secs=1,
+            summary_writer=train_summ_writer)
 
         print('Finished training. Final batch loss %d' % final_loss)
 
