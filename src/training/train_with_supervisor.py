c5cb6f62d6774f8f8032b0fd5dce4a1655851b5b
Zhenxing Zhang <Zhenxingzhang@users.noreply.github.com>
2018-02-05 23:12:32 +0000

    update config

--- src/training/train_with_supervisor.py
+++ src/training/train_with_supervisor.py
@@ -50,8 +50,8 @@ def run(config):
 
         # Define the scopes that you want to exclude for restoration
         # exclude = ['InceptionResnetV2/Logits', 'InceptionResnetV2/AuxLogits']
-        exclude = ["InceptionV1/Logits", "InceptionV1/AuxLogits"]
-        variables_to_restore = slim.get_variables_to_restore(exclude=exclude)
+        # exclude = ["InceptionV1/Logits", "InceptionV1/AuxLogits"]
+        variables_to_restore = slim.get_variables_to_restore(exclude=arg_config.EXCLUDE_NODES)
 
         # Performs the equivalent to tf.nn.sparse_softmax_cross_entropy_with_logits but enhanced with checks
         loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,6 @@
 import yaml
 from dictionaries import Dict
+import argparse
 
 
 def parse_config_file(config_filename):
@@ -11,6 +12,7 @@ def parse_config_file(config_filename):
     arg_coonfig.PRETAIN_MODEL = str(cfg["MODEL"]["PRETRAIN_MODEL"])
     arg_coonfig.PRETAIN_MODEL_PATH = str(cfg["MODEL"]["PRETAIN_MODEL_PATH"])
     arg_coonfig.PRETAIN_MODEL_URL = str(cfg["MODEL"]["PRETAIN_MODEL_URL"])
+    arg_coonfig.EXCLUDE_NODES = cfg["MODEL"]["EXCLUDE_NODES"]
     arg_coonfig.INPUT_HEIGHT = int(cfg["MODEL"]["INPUT_HEIGHT"])
     arg_coonfig.INPUT_WIDTH = int(cfg["MODEL"]["INPUT_WIDTH"])
     arg_coonfig.CATEGORIES = int(cfg["MODEL"]["CLASSES"])
@@ -27,3 +29,14 @@ def parse_config_file(config_filename):
     arg_coonfig.EVAL_TF_RECORDS = str(cfg["EVAL"]["TF_RECORDS"])
 
     return arg_coonfig
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Default argument')
+    parser.add_argument('-c',
+                        dest="config_filename", type=str, required=True,
+                        help='the config file name must be provide')
+    args = parser.parse_args()
+
+    arg_config = parse_config_file(args.config_filename)
+    print(type(arg_config.EXCLUDE_NODES))
