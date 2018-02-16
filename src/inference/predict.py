e2dab3d7fc192b2b021bddb72e533d56f012c656
Zhenxing Zhang <Zhenxingzhang@users.noreply.github.com>
2018-02-16 00:13:28 +0000

    fix predict

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -6,6 +6,7 @@ from sklearn.metrics import precision_recall_fscore_support as score
 import sys
 sys.path.append("/data/slim/models/research/slim/")
 from datasets import flowers
+from itertools import izip
 
 import os
 import sys
@@ -15,13 +16,13 @@ import tensorflow as tf
 import csv
 
 
-def predict_set(config):
-    model_path = os.path.join('/data/checkpoints/flowers/', config.MODEL_NAME, str(config.TRAIN_LEARNING_RATE))
+def predict_set(config_):
+    model_path = os.path.join('/data/checkpoints/flowers/', config_.MODEL_NAME, str(config_.TRAIN_LEARNING_RATE))
     if not os.path.exists(model_path):
         print("Model not exist: {}".format(model_path))
         exit()
 
-    output_path = os.path.join(config.EVAL_OUTPUT, config.MODEL_NAME)
+    output_path = os.path.join(config_.EVAL_OUTPUT, config_.MODEL_NAME)
     if not os.path.exists(output_path):
         os.mkdir(output_path)
     eval_output = os.path.join(output_path, "eval_results.csv")
@@ -30,15 +31,15 @@ def predict_set(config):
         tf.logging.set_verbosity(tf.logging.INFO)  # Set the verbosity to INFO level
 
         # First create the dataset and load one batch
-        valid_dataset = flowers.get_split('validation', config.TRAIN_TF_RECORDS)
+        valid_dataset = flowers.get_split('validation', config_.TRAIN_TF_RECORDS)
         images, labels = dataset.load_batch(valid_dataset,
-                                            batch_size=config.TRAIN_BATCH_SIZE,
-                                            height=config.INPUT_HEIGHT,
-                                            width=config.INPUT_WIDTH,
+                                            batch_size=config_.TRAIN_BATCH_SIZE,
+                                            height=config_.INPUT_HEIGHT,
+                                            width=config_.INPUT_WIDTH,
                                             epochs=1)
         # Create the model inference
         net_fn = nets_factory.get_network_fn(
-            config.PRETAIN_MODEL,
+            config_.PRETAIN_MODEL,
             dataset.num_classes,
             is_training=False)
 
@@ -56,18 +57,22 @@ def predict_set(config):
 
         with tf.Session() as sess, open(eval_output, 'w') as f:
             print("writing results to {}".format(eval_output))
-            with slim.queues.QueueRunners(sess):
-                sess.run(tf.local_variables_initializer())
-                init_fn(sess)
-                print("Restore model from: {}".format(model_path))
+            init_fn(sess)
+            print("Restore model from: {}".format(model_path))
 
-                writer = csv.writer(f)
-                writer.writerow(["y_true", "preds"])
-                while True:
-                    y_true, preds = sess.run([labels, predictions])
-                    results = zip(y_true, preds)
-                    writer.writerows("{}, {}".format(row[0], row[1]) for row in results)
-                    print("precessing {} records".format(str(y_true.shape[0])))
+            sess.run(tf.initialize_local_variables())
+
+            writer = csv.writer(f)
+            writer.writerow(["y_true", "preds"])
+
+            try:
+                with slim.queues.QueueRunners(sess):
+                    while True:
+                        y_true, preds = sess.run([labels, predictions])
+                        writer.writerows(zip(y_true, preds))
+                        print("precessing {} records".format(str(y_true.shape[0])))
+            except tf.errors.OutOfRangeError:
+                print('')
         print("Prediction finished.")
 
 
